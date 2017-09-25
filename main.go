ce6696c838843d9529133d6ccb2aa7a4431c1aee
barum <barumsumni@gmail.com>
2017-09-25 16:01:04 -0500

    faker changes

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	sc "strconv"
+
+	"github.com/icrowley/fake"
+)
+
+func main() {
+
+	fileWriterPTR, ferr := os.Create("testdata")
+	if ferr != nil {
+		fmt.Println("file creation error", ferr)
+	}
+	defer fileWriterPTR.Close()
+
+	err := fake.SetLang("en")
+	if err != nil {
+		panic(err)
+	}
+	//password := fake.SimplePassword()
+
+	for i := 0; i < 10; i++ {
+
+		ssn := fake.DigitsN(9)
+		phone := fake.DigitsN(3) + "-" + fake.DigitsN(3) + "-" + fake.DigitsN(4)
+		city := fake.City()
+		state := fake.State()
+		address := fake.StreetAddress()
+		s, _ := sc.Atoi(ssn)
+		firstname := fake.MaleFirstName()
+		if s%2 == 0 {
+			firstname = fake.FemaleFirstName()
+		}
+
+		lastname := fake.LastName()
+
+		fmt.Fprintf(fileWriterPTR, "%s,%s,%s,%s,%s,%s,%s\n", ssn, phone, city, state, address, firstname, lastname)
+	}
+
+}
